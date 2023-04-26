5a8489155131d4a70af6deddb037f728bb545e34
Rouven Himmelstein <rouven@himmelstein.info>
2023-04-26 15:48:12 +0200

    feat: bump jaudiotagger version

--- src/main/java/de/itlobby/discoverj/Main.java
+++ src/main/java/de/itlobby/discoverj/Main.java
@@ -10,24 +10,12 @@ import de.itlobby.discoverj.util.VersionDetector;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.Logger;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.IOException;
-import java.util.logging.LogManager;
 
 import static de.itlobby.discoverj.util.SystemUtil.DISCOVERJ_TEMP_DIR;
 
 public class Main {
-    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(Main.class);
-
-    static {
-        // disable java.util.logging
-        LogManager.getLogManager().reset();
-        // redirect to log4j
-        SLF4JBridgeHandler.install();
-    }
-
     public static void main(String[] args) throws IOException {
         if (DISCOVERJ_TEMP_DIR.exists()) {
             FileUtils.deleteDirectory(DISCOVERJ_TEMP_DIR);
